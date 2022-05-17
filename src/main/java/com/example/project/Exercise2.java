a8cc534aa29cd1d7946b23532e998543e6de6e8c
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2022-05-16 21:49:10 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -25,7 +25,51 @@ public class Exercise2 {
 	}
 
 	public boolean esSubconjuntoSumaExt(int a[], int suma) {
+		 
+		int posicion=0;
+		 for (int i = 0; i < a.length; i++) {
+			 if(a[i]==7) {
+			 posicion=i;
+			 }
+		 }
+		if(a[posicion+1]==1){
+				System.out.println("no pueden almacenarse el 7");
+				if((esSubconjuntoSuma(eliminar(a,posicion),0,suma)==true)) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+		else {
+			if((esSubconjuntoSuma(a,1,suma)==true)) {
+				return true;
+			}
+		}
 
 		return false;
 	}
+ boolean esSubconjuntoSuma(int conjunto[], int n, int suma){
+	// Casos base
+	if (suma == 0)
+	return true;
+	if (n == conjunto.length)
+	return false;
+	/* verificamos si conseguimos la suma por alguna de las siguientes formas:
+	(a) incluyendo el primer elemento
+	(b) excluyendo el primer elemento */
+	return esSubconjuntoSuma(conjunto, n + 1, suma - conjunto[n])
+	|| esSubconjuntoSuma(conjunto, n + 1, suma );
+	}
+	 
+	 
+ int[] eliminar(int a[], int pos) {
+		
+		 for (int k = pos; k < a.length-1; k++) {
+			a[k]=a[k+1];
+		}
+
+		 return a;
+	 }
+}
 }
